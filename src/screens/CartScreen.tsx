850affc4c08eb0de3bc3c90760cff35c10b41e97
sarim <mirzasarim03@gmail.com>
2024-09-29 00:20:35 +0500

    Changed Text Color of Screen testing

--- src/screens/CartScreen.tsx
+++ src/screens/CartScreen.tsx
@@ -1,17 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { COLORS } from '../theme/theme'
 
 const CartScreen = () => {
   return (
     <View>
-      <Text>CartScreen</Text>
+      <Text style={styles.textColor}>CartScreen</Text>
     </View>
   )
 }
 
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textColor:{
+    color:COLORS.primaryBlackHex,
+  }
+
+})
 
 
 
--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,16 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { COLORS } from '../theme/theme'
 
 const DetailsScreen = () => {
   return (
     <View>
-      <Text>DetailsScreen</Text>
+      <Text style={styles.textColor}>DetailsScreen</Text>
     </View>
   )
 }
 
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textColor:{
+    color:COLORS.primaryBlackHex,
+  }
+})
 
 export default DetailsScreen
