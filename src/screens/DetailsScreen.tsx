0fb57b28cc977b46af80f3835c00d1c216de297a
sarim <mirzasarim03@gmail.com>
2024-10-10 16:19:29 +0500

    Some Additions  in Details Screen

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,6 +1,6 @@
-import { ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
 import React, { useState } from 'react'
-import { COLORS } from '../theme/theme'
+import { COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme'
 import { useStore } from '../store/store'
 import ImageBackgroundInfo from '../components/ImageBackgroundInfo'
 
@@ -20,6 +20,10 @@ const DetailsScreen = ({navigation,route} : any ) => {
     id : string
    ) =>{ favourite ? deleteFromFavouriteList(type,id) : addToFavouriteList(type,id)
   };
+
+  const [price , setprice] = useState(ItemOfIndex.prices[0]);
+
+  const [fullDesc,setFullDesc] = useState(false)
   
 
  const BackHandler = () =>{
@@ -46,7 +50,38 @@ const DetailsScreen = ({navigation,route} : any ) => {
         ratings_count = {ItemOfIndex.ratings_count}
         roasted = {ItemOfIndex.roasted}
         BackHandler = {BackHandler}
-        ToggleFavourite = {ToggleFavourite}/>
+        ToggleFavourite = {ToggleFavourite}
+      />
+      <View style={styles.footerInfoArea}>
+        <Text style={styles.InfoTitle}>Description</Text>
+        {fullDesc ? (
+          <TouchableWithoutFeedback onPress={() => {setFullDesc(prev => !prev)}} >
+            <Text style={styles.descriptionText}>{ItemOfIndex.description}</Text>
+          </TouchableWithoutFeedback>
+         ) : ( 
+         <TouchableWithoutFeedback 
+           onPress={() => {
+            setFullDesc(prev => !prev)
+            }}>
+          <Text numberOfLines={3} style={styles.descriptionText}>{ItemOfIndex.description}</Text>
+         </TouchableWithoutFeedback>)}
+         <Text style={styles.InfoTitle}>Size</Text>
+         <View style={styles.SizeOuterContainer}>
+          {ItemOfIndex.prices.map((data : any ) => (
+            <TouchableOpacity  key={data.size} style={styles.sizeBox}>
+              <Text 
+                style={[
+                  styles.sizeText,
+                  {
+                    fontSize:
+                     ItemOfIndex.type=="bean" 
+                     ? FONTSIZE.size_20 
+                     : FONTSIZE.size_16,},]}>{data.size}</Text>
+            </TouchableOpacity>
+          ) )}
+         </View>
+      </View>
+      
       </ScrollView>
     </View>
   )
@@ -61,7 +96,33 @@ const styles = StyleSheet.create({
   },
   ScrollViewFlex:{
     flexGrow:1,
-  }
+  },
+  footerInfoArea:{
+    padding:SPACING.space_20,
+
+  },
+  InfoTitle:{
+    fontFamily:FONTFAMILY.poppins_semibold,
+    fontSize:FONTSIZE.size_16,
+    color:COLORS.primaryWhiteHex,
+    marginBottom:SPACING.space_10,
+  },
+  descriptionText:{
+    letterSpacing:0.5,
+    fontFamily:FONTFAMILY.poppins_regular,
+    fontSize:FONTSIZE.size_14,
+    color:COLORS.primaryWhiteHex,
+    marginBottom:SPACING.space_30,
+  },
+  SizeOuterContainer:{
+
+  },
+  sizeText:{
+
+  },
+  sizeBox:{
+
+  },
 })
 
 export default DetailsScreen
